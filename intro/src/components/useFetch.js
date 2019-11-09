5a28ca9a6b2d0b48ba2a3b941d40c8e95e9f8cfe
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-11-09 21:18:26 +0300

    9.11

--- intro/src/components/useFetch.js
+++ intro/src/components/useFetch.js
@@ -13,25 +13,10 @@ export default async function useFetcch(id, setUs, setLoading, dep) {
                 setUs(list)
             } catch (e) {
                 console.error(e)
-            } finally{
+            } finally {
                 setLoading(false);
             }
         }
         fetchUsers()
     }, [dep]);
-
-
-
-
-    /*  useEffect(() => {
-        try {
-            fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${id}.json`)
-                .then((response) =>
-                    response.json()
-                ).then(list => setUs(list))
-
-        } catch (e) {
-            console.error(e);
-        }
-    }, [dep]); */
 }
